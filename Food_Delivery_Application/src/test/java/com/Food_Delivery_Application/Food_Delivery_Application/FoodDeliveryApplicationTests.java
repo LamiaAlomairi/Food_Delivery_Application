786666d7951131ab9e8b79aaaa9785aaa9e92447
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-01 15:15:07 +0400

    Create Food_Delivery_Application project and packages

---
+++ Food_Delivery_Application/src/test/java/com/Food_Delivery_Application/Food_Delivery_Application/FoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Food_Delivery_Application.Food_Delivery_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
